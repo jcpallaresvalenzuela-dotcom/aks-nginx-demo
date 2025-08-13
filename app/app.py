99edf2bd49b8a6a2d6a341891f892519e1d781b9
JuanC <jcpallaresvalenzuela@gmail.com>
2025-08-13 11:08:23 +0200

    add uvicorn server && add setup nginx sh - change dockerfile

---
+++ app/app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request
+from datetime import datetime
+import socket
+
+app = FastAPI()
+
+@app.get("/info")
+async def get_info(request: Request):
+    return {
+        "mensaje": "Hola desde la app Python integrada con Nginx 🚀",
+        "fecha_hora": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "ip_cliente": request.client.host,
+        "hostname_servidor": socket.gethostname(),
+        "version_app": "1.0.0"
+    }
---
+++ app/setup_nginx.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+# Crear carpeta para logs
+mkdir -p /var/log/nginx
+
+# Archivo HTML básico
+cat <<EOF > /usr/share/nginx/html/index.html
+<!DOCTYPE html>
+<html>
+<body>
+<p>Desplegado con GitHub Actions + Ansible</p>
+</body>
+</html>
+EOF
+
+# Configuración de básica de nginx 
+cat <<EOF > /etc/nginx/conf.d/default.conf
+server {
+    listen 80;
+    server_name _;
+    
+    # Sirve archivos estáticos
+    location / {
+        root /usr/share/nginx/html;
+        index index.html;
+    }
+
+    # Redirige /info hacia el servicio Python en el puerto 8000
+    location /info {
+        proxy_pass http://127.0.0.1:8000/info;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+    }
+
+    access_log /var/log/nginx/access.log;
+    error_log /var/log/nginx/error.log;
+}
+EOF
