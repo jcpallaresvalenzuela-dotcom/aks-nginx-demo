e253fe088b88e79c826bbcc4edaf9cc6f6bd5443
JuanC <jcpallaresvalenzuela@gmail.com>
2025-08-13 11:25:43 +0200

    change behavior of nginx & dockerfile

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 @app.get("/info")
 async def get_info(request: Request):
     return {
-        "mensaje": "Hola desde la app Python integrada con Nginx 🚀",
+        "mensaje": "app Python integrada con Nginx 🚀",
         "fecha_hora": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "ip_cliente": request.client.host,
         "hostname_servidor": socket.gethostname(),
--- app/setup_nginx.sh
+++ app/setup_nginx.sh
@@ -29,8 +29,8 @@ server {
     # Redirige /info hacia el servicio Python en el puerto 8000
     location /info {
         proxy_pass http://127.0.0.1:8000/info;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
     }
 
     access_log /var/log/nginx/access.log;
